8695b4c1c5d82a6ebf0419ccd78ebaef3b98e928
Siebe Keuppens <78792436+SiebeKeuppens@users.noreply.github.com>
2022-02-16 16:42:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://objective-mahavira-627ad1.netlify.app/'
+        origin: 'https://objective-mahavira-627ad1.netlify.app'
     })
 );
 app.options('*', cors());
